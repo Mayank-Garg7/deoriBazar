0e8711cce2ef55f88854a788f9c8d4c68d871905
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-06-09 14:00:22 +0530

    chore update the files remove the errors

--- frontend/src/components/feature/filter.tsx
+++ frontend/src/components/feature/filter.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function filter() {
+function Filter() {
   return (
     <div>
       
@@ -8,4 +8,4 @@ function filter() {
   )
 }
 
-export default filter
+export default Filter
